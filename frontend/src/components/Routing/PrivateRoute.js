fe0aeaec5c15db389e14332d5e133cfcf782f89b
MrJefferson29 <thejefferson29@gmail.com>
2025-01-29 05:58:03 +0100

    first commit

--- frontend/src/components/Routing/PrivateRoute.js
+++ frontend/src/components/Routing/PrivateRoute.js
@@ -21,7 +21,7 @@ const PrivateRoute =( ) => {
             },
         };
         try {
-            const { data } = await axios.get("https://ilan.onrender.com/auth/private", config); 
+            const { data } = await axios.get("https://sparko-tracking.onrender.com/auth/private", config); 
 
             setAuth(true)
             setActiveUser(data.user)
