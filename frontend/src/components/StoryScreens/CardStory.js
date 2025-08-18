5ecf9b855a9276e98724749c6a6e34f64a5c699b
MrJefferson29 <100526664+MrJefferson29@users.noreply.github.com>
2025-08-18 13:56:08 +0800

    Update CardStory.js

--- frontend/src/components/StoryScreens/CardStory.js
+++ frontend/src/components/StoryScreens/CardStory.js
@@ -183,8 +183,8 @@ const Story = ({ story }) => {
               <p className="contact-info">
                 <strong>Contact Information:</strong> For inquiries, contact
                 our support at{" "}
-                <a href="mailto:albatrossexpresslogistics@gmail.com">
-                  albatrossexpresslogistics@gmail.com
+                <a href="mailto:arbatrossdelivery@gmail.com">
+                  arbatrossdelivery@gmail.com
                 </a>
                 .
               </p>
